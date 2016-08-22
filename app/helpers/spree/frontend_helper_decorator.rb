7e8c221bb71b481e0cfbcccd53a98b2f0cc8f651
Tim Hogg <thogg4@gmail.com>
2016-08-22 15:58:29 -0600

    allow 2 levels in dropdown

--- app/helpers/spree/frontend_helper_decorator.rb
+++ app/helpers/spree/frontend_helper_decorator.rb
@@ -1,14 +1,22 @@
 Spree::FrontendHelper.module_eval do
 
   def menu_goes_here
-    content_tag :ul, class: 'menu nav navbar-nav' do
+    content_tag :ul, class: 'menu' do
       Spree::Taxonomy.where(in_menu: true).map do |taxonomy|
         root_taxon = taxonomy.root
-        content_tag :li, class: 'dropdown' do
-          anchor = link_to(root_taxon.name, seo_url(root_taxon), class: 'dropdown-toggle', data: { toggle: 'dropdown' })
-          dropdown = content_tag :ul, class: 'dropdown-menu' do
+        content_tag :li do
+          anchor = link_to(root_taxon.name, seo_url(root_taxon))
+          dropdown = content_tag :ul do
             root_taxon.children.where(in_menu: true).map do |taxon|
-              content_tag(:li, link_to(taxon.name, seo_url(taxon)))
+              content_tag :li do 
+                anchor = link_to(taxon.name, seo_url(taxon))
+                dropdown = content_tag :ul do
+                  taxon.children.where(in_menu: true).map do |taxon|
+                    content_tag(:li, link_to(taxon.name, seo_url(taxon)))
+                  end.join("\n").html_safe
+                end
+                taxon.children.where(in_menu: true).empty? ? anchor : anchor + dropdown
+              end
             end.join("\n").html_safe
           end
           anchor + dropdown
